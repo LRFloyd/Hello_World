2925f5564cbeddc3989174208aff84d4d5088646
LRFloyd <lavelle.floyd@gmail.com>
2020-08-21 11:11:34 -0400

    add & use conditionals

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -85,7 +85,42 @@ namespace HelloWorld
             //Console.WriteLine(ageTwo);
             //ageTwo--;
 
-            
+            //Part eight concatenation example
+            //Console.WriteLine(messageOne + " " + messageTwo + ".");
+            //Console.WriteLine("I am " + ageOne + " years old, and it is true that " + messageTwo + ".");
+            //Console.WriteLine("I weigh around " + weightOne + " pounds, and I am around " + heightOne + " inches tall.");
+            //Console.WriteLine("My father is {0} years old, and it is {1} that he is a human.", ageThree, isHumanLikeOne);
+            //Console.WriteLine("I am {0} years old, and it is true that {1}.", ageOne, messageTwo);
+            //Console.WriteLine("I weigh around {0} pounds, and I am around {1} inches tall.", weightOne, heightOne);
+
+            ////Part nine string equality examples
+            //Console.WriteLine(messageTwo.Equals(messageOne));
+            //Console.WriteLine(messageTwo.Equals("I'm a Developer"));
+
+            //Part ten string length examples
+            //int messOneLength = messageOne.Length;
+            //int messTwoLength = messageTwo.Length;
+
+            //Console.WriteLine("The length of messageOne is " + messageOne.Length);
+            //Console.WriteLine("The length of messageTwo is " + messageTwo.Length);
+            //Console.WriteLine("The length of messageOne is " + messOneLength);
+            //Console.WriteLine("The length of messageTwo is " + messTwoLength);
+
+            //Part 11: Conditionals examples
+            //string city1 = "Vesuvius";
+            //string city2 = "Nola";
+            //string city3 = "Nuceria";
+            //int city1Distance, city2Distance, city3Distance;
+
+            //Solicit user input
+            //Console.WriteLine("What is the distance to {0}?", city1);
+            //city1Distance = int.Parse(Console.ReadLine());
+            //Console.WriteLine("What is the distance to " + city2 + "?");
+            //city2Distance = int.Parse(Console.ReadLine());
+            //Console.WriteLine("What is the distance to Nuceria?");
+            //city3Distance = int.Parse(Console.ReadLine());
+
+
 
         
     }
