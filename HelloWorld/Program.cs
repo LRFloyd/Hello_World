48a7922af4a9f1aae8796cb92b2b86eb13dfabf3
LRFloyd <lavelle.floyd@gmail.com>
2020-08-21 11:10:19 -0400

    use operators

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -40,29 +40,52 @@ namespace HelloWorld
 
             ////Part two set initial Developer attributes
             int ageOne = 34;
-            int ageTwo = 45;
-            int ageThree = 80;
-            double heightOne = 72.50;
-            double heightTwo = 91.45;
-            float weightOne = 210.028461f;
-            float weightTwo = 331.123457f;
+            //int ageTwo = 45;
+            //int ageThree = 80;
+            //double heightOne = 72.50;
+            //double heightTwo = 91.45;
+            //float weightOne = 210.028461f;
+            //float weightTwo = 331.123457f;
             bool isHumanLikeOne = true;
-            bool isHumanLikeTwo = false;
-            char genderMale = 'M';
-            char genderFemale = 'F';
+            //bool isHumanLikeTwo = false;
+            //char genderMale = 'M';
+            //char genderFemale = 'F';
 
             ////Part three displays variable values
-            Console.WriteLine(messageOne);//displays intial greeting to console
-            Console.WriteLine(messageTwo);
+            //Console.WriteLine(messageOne);//displays intial greeting to console
+            //Console.WriteLine(messageTwo);
 
             ////Part four mathematical operator, bool and char examples
-            Console.WriteLine(ageOne + ageTwo);//Addition example
-            Console.WriteLine(heightOne - heightTwo);//Subtraction example
-            Console.WriteLine(weightOne * weightTwo);//Multiplication example
-            Console.WriteLine(isHumanLikeOne);//Bool example
-            Console.WriteLine(genderMale);//Char example
+            //Console.WriteLine(ageOne + ageTwo);//Addition example
+            //Console.WriteLine(heightOne - heightTwo);//Subtraction example
+            //Console.WriteLine(weightOne * weightTwo);//Multiplication example
+            //Console.WriteLine(isHumanLikeOne);//Bool example
+            //Console.WriteLine(genderMale);//Char example
 
-         
+            ////Part five variable reassignment
+            //ageTwo = 70;
+            //heightTwo = 35.12345f;
+            //weightTwo = -429.1234573f;
+
+            ////Part six mathematical operator examples
+            //Console.WriteLine(ageOne + ageTwo);
+            //Console.WriteLine(heightOne - heightTwo);
+            //Console.WriteLine(weightOne * weightTwo);
+            //Console.WriteLine(ageTwo / ageOne);
+            //Console.WriteLine(ageTwo % ageOne);
+            //Console.WriteLine(ageThree % ageOne);
+            //Console.WriteLine(ageThree / ageOne);
+
+            ////Part seven increment and decrement operator examples
+            //ageTwo = ageTwo + 1;
+            //Console.WriteLine(ageTwo);
+            //ageTwo++;
+            //Console.WriteLine(ageTwo);
+            //ageTwo--;
+            //Console.WriteLine(ageTwo);
+            //ageTwo--;
+
+            
 
         
     }
